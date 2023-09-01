9cd29a17aa125af217556aaae7c8fa139135bf07
Angel Santana <anffernando01@gmail.com>
2023-09-01 02:10:03 -0300

    Proyecto utn + base de datos

--- frontend-project/src/views/Shop.jsx
+++ frontend-project/src/views/Shop.jsx
@@ -8,25 +8,24 @@ function Shop() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8080/admin/products'); // Reemplaza 'URL_DEL_SERVIDOR' por la URL de tu API
+        const response = await fetch('http://localhost:8080/admin/products');
         const { msg, result } = await response.json();
-        setData(result); // Asumimos que el resultado es un array con los productos
+        setData(result);
       } catch (error) {
         console.error("Hubo un error obteniendo los datos:", error);
       }
     }
 
     fetchData();
-  }, []); // El array vacío indica que el useEffect se ejecutará solo una vez cuando el componente se monte
+  }, []);
 
   return (
     <>
       <main className="productos productos__contenedor">
         <h2 className="productos__heading">Nuestra Colección</h2>
         <div className="productos__grid">
-          {/* Iteramos sobre el array de productos y renderizamos cada uno */}
           {data.map(producto => (
-            <div className="producto" key={producto.id}> {/* Asumimos que cada producto tiene un 'id' único */}
+            <div className="producto" key={producto.id}>
               <img className="producto__imagen" src={producto.image} alt="imagen guitarra" />
               <div className="producto__contenido">
                 <h3 className="producto__nombre">{producto.name}</h3>
